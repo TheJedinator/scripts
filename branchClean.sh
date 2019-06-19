22ac9c9ae4a1b92893499f052d1ebc1b39c09854
Jed Palmater <TheJedinator@users.noreply.github.com>
2019-06-19 10:01:17 -0300

    Update branchClean.sh

--- branchClean.sh
+++ branchClean.sh
@@ -2,7 +2,7 @@
 git prune && git remote prune origin && git remote prune upstream
 
 # Clean up Branches that have been merged 
-git branch --merged | egrep -v "(^\*|master|develop)" | xargs git branch -D
+git branch --merged | egrep -v "(^\*|master|develop|dev)" | xargs git branch -D
 
 # Clean branches that are gone 
 git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -D
