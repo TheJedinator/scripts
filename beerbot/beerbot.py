9c08422b3e8bd1ec1bccaa5d4876c94cfcf7de9e
Jed Palmater <jed.palmater@eigen.io>
2019-10-14 16:39:04 -0300

    - ignore ide folders - Integrate with api

--- beerbot/beerbot.py
+++ beerbot/beerbot.py
@@ -8,6 +8,7 @@ import logging
 import urllib
 import requests
 import datetime
+import json
 from bs4 import BeautifulSoup
 
 # Grab the Bot OAuth token from the environment.
@@ -34,10 +35,11 @@ def lambda_handler(data, context):
         # and reverse it.
         text = slack_event["text"]
         response = ""
+        text = text.lower()
         if "create poll" in text:
-          response = create_poll()
+            response = create_poll()
         else:
-          response = text[::-1]
+            response = text[::-1]
 
         # Get the ID of the channel where the message was posted.
         channel_id = slack_event["channel"]
@@ -76,27 +78,49 @@ def lambda_handler(data, context):
     # Everything went fine.
     return "200 OK"
 
+
 def create_poll():
-  req = requests.get('https://www.anbl.com/growlers')
-  data = req.content
-  soup = BeautifulSoup(data, 'lxml')
-  york_fred = soup.find('div', attrs={'class': 'widget', 'data-type': 'view', 'data-index': '3'})
-  #print(york_fred.prettify())
-  test_list = []
+    req = requests.get('https://www.anbl.com/growlers')
+    data = req.content
+    soup = BeautifulSoup(data, 'lxml')
+    york_fred = soup.find(
+        'div', attrs={'class': 'widget', 'data-type': 'view', 'data-index': '3'})
+    # print(york_fred.prettify())
+    test_list = []
 
-  for bt in york_fred.find_all('strong'):
-      x = bt.get_text().strip()
-      if x != "":
-        # print(x)
-        test_list.append(x)
-  polly_str = ""
-  date = datetime.date.today().strftime('%B %d, %Y')
-  if len(test_list) <= 0:
-    polly_str = "Either the ANBL Devs forgot how to use p-tags again OR there is a special event, I'm not that smart, you'll have to manage on your own today....... https://www.anbl.com/growlers"
-  else:
-    polly_str = '/polly "On Tap - {}"'.format(date)
+    for bt in york_fred.find_all('strong'):
+        x = bt.get_text().strip()
+        if x != "":
+            # print(x)
+            test_list.append(x)
+    polly_str = ""
+    date = datetime.date.today().strftime('%B %d, %Y')
+    if len(test_list) <= 0:
+        polly_str = "Either the ANBL Devs forgot how to use p-tags again OR there is a special event, I'm not that smart, you'll have to manage on your own today....... https://www.anbl.com/growlers"
+    else:
+        polly_str = '/polly "On Tap - {}"'.format(date)
 
-  for beer in test_list:
-      polly_str += ' "{}"'.format(beer)
-
-  return polly_str
+    # For each beer, make a request to the untapped API for some info
+    # If the request succeeds we add info & beer to the poll, otherwise we just add the beer to the poll
+    for beer in test_list:
+        api_str = """ https://api.untappd.com/v4/search/beer?q={}&client_id=FAC6C2CDD00FD0787888C1A4383832B52D20463F&client_secret=091BCEE693FF092F66CF5A6381077940E541CC82""".format(beer)
+        res = requests.get(api_str)
+        res = json.loads(res.content)
+        beer_str = None
+        if res.get('status_code') == 200:
+            search_results = res.get('response').get('beers').get('items')
+            sr = search_results[0]
+            sr = sr['beer']
+            beer_str = """Name: {}
+            Style: {}
+            IBU: {}
+            ABV: {}
+            Description: {}
+            """.format(sr['beer_name'], sr['beer_style'], sr['beer_ibu'], sr['beer_abv'], sr['beer_description'])
+            print(beer_str)
+        if beer_str: 
+            polly_str += ' "{}{}"'.format(beer, beer_str)
+        else: 
+            polly_str += ' "{}"'.format(beer)
+    print(polly_str)
+    return polly_str
--- beerbot/whatsontapp.py
+++ beerbot/whatsontapp.py
@@ -1,27 +1,56 @@
 import requests
 import datetime
 from bs4 import BeautifulSoup
+from pprint import pprint
+import json
 
-req = requests.get('https://www.anbl.com/growlers')
-data = req.content
-soup = BeautifulSoup(data, 'lxml')
-york_fred = soup.find('div', attrs={'class': 'widget', 'data-type': 'view', 'data-index': '3'})
-print(york_fred.prettify())
-test_list = []
 
-for beer in york_fred.find_all('p'):
-  for bt in beer.find_all('strong'):
-    print(bt.get_text())
-    x = bt.get_text().strip()
-    test_list.append(x)
-polly_str = ""
-date = datetime.date.today().strftime('%B %d, %Y')
-if len(test_list) <= 0:
-  polly_str = "Either the ANBL Devs forgot how to use p-tags again OR there is a special event, I'm not that smart, you'll have to manage on your own today......."
-else:
-  polly_str = '/polly "On Tap - {}"'.format(date)
+def main():
+    req = requests.get('https://www.anbl.com/growlers')
+    data = req.content
+    soup = BeautifulSoup(data, 'lxml')
+    york_fred = soup.find(
+        'div', attrs={'class': 'widget', 'data-type': 'view', 'data-index': '3'})
+    # print(york_fred.prettify())
+    test_list = []
 
-for beer in test_list:
-    polly_str += ' "{}"'.format(beer)
+    for bt in york_fred.find_all('strong'):
+        x = bt.get_text().strip()
+        if x != "":
+            # print(x)
+            test_list.append(x)
+    polly_str = ""
+    date = datetime.date.today().strftime('%B %d, %Y')
+    if len(test_list) <= 0:
+        polly_str = "Either the ANBL Devs forgot how to use p-tags again OR there is a special event, I'm not that smart, you'll have to manage on your own today....... https://www.anbl.com/growlers"
+    else:
+        polly_str = '/polly "On Tap - {}"'.format(date)
 
-print(polly_str)
+    # For each beer, make a request to the untapped API for some info
+    # If the request succeeds we add info & beer to the poll, otherwise we just add the beer to the poll
+    for beer in test_list:
+        api_str = """ https://api.untappd.com/v4/search/beer?q={}&client_id=FAC6C2CDD00FD0787888C1A4383832B52D20463F&client_secret=091BCEE693FF092F66CF5A6381077940E541CC82""".format(beer)
+        res = requests.get(api_str)
+        res = json.loads(res.content)
+        beer_str = None
+        if res.get('status_code') == 200:
+            search_results = res.get('response').get('beers').get('items')
+            sr = search_results[0]
+            sr = sr['beer']
+            beer_str = """Name: {}
+            Style: {}
+            IBU: {}
+            ABV: {}
+            Description: {}
+            """.format(sr['beer_name'], sr['beer_style'], sr['beer_ibu'], sr['beer_abv'], sr['beer_description'])
+            print(beer_str)
+        if beer_str: 
+            polly_str += ' "{}{}"'.format(beer, beer_str)
+        else: 
+            polly_str += ' "{}"'.format(beer)
+    print(polly_str)
+    return polly_str
+
+
+if __name__ == "__main__":
+    main()
