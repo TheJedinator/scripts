ddad072c617fea34fbbb780181a0c82f1c0d8591
Jed Palmater <jed.palmater@eigen.io>
2019-11-23 17:40:01 -0400

    clean up branch clean script

--- branchClean.sh
+++ branchClean.sh
@@ -1,8 +1,8 @@
 # Prune first 
-#git prune && git remote prune origin && git remote prune upstream
+git prune && git remote prune origin && git remote prune upstream
 
 # Clean up Branches that have been merged 
-git branch --merged | egrep -v "(^\*|master|dev|develop)" | xargs git branch -D
+git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -D
 
 # Clean branches that are gone 
-git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs -r git branch -D
+git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs git branch -D
