d9913679052ab8b66ff8c65adb61fd6b66fc60a3
Jed Palmater <jed.palmater@eigen.io>
2019-11-23 19:14:59 -0400

    - commented out the actual delete line, for safety - changed from sed to grep, cause I'm dumb - was actually running it so changed dates

--- remote_delete_branches.sh
+++ remote_delete_branches.sh
@@ -1,7 +1,10 @@
-for k in $(git branch -r | sed /\*/d); do 
-  if [ -z "$(git log -1 --since='Mar 10, 2019' -s $k)" ]; then
+checkStr="/origin/develop"
+# for k in $(git branch -r | sed /master\*/d); do 
+for k in $(git branch -r | grep -vwE "(master|develop)"); do 
+  if [ -z "$(git log -1 --since='Oct 30, 2019' -s $k)" ]; then
     branch_name_with_no_origin=$(echo $k | sed -e "s/origin\///")
     echo deleting branch: $branch_name_with_no_origin
-    git push origin --delete $branch_name_with_no_origin
+    # UNCOMMENT THE LINE BELOW TO ACTUALLY DELETE
+    # git push origin --delete $branch_name_with_no_origin
   fi
 done
