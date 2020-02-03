b4375da2fe17049d035695cadd0dfe0788e9208d
Jed Palmater <jed.palmater@eigen.io>
2020-02-03 17:59:00 -0400

    delete beerbot

--- remote_delete_branches.sh
+++ remote_delete_branches.sh
@@ -1,7 +1,7 @@
 checkStr="/origin/develop"
 # for k in $(git branch -r | sed /master\*/d); do 
 for k in $(git branch -r | grep -vwE "(master|develop)"); do 
-  if [ -z "$(git log -1 --since='Oct 30, 2019' -s $k)" ]; then
+  if [ -z "$(git log -1 --since='Jan 30, 2019' -s $k)" ]; then
     branch_name_with_no_origin=$(echo $k | sed -e "s/origin\///")
     echo deleting branch: $branch_name_with_no_origin
     # UNCOMMENT THE LINE BELOW TO ACTUALLY DELETE
