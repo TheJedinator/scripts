ad80b238aef12d76ba9542f46465e3932c94babf
Jed Palmater <TheJedinator@users.noreply.github.com>
2024-04-05 12:10:39 -0300

    update branchclean

--- branchClean.sh
+++ branchClean.sh
@@ -1,8 +1,11 @@
 # Prune first 
-git prune && git remote prune origin && git remote prune upstream
+echo "pruning...."
+git prune && git remote prune origin
 
 # Clean up Branches that have been merged 
+echo "cleaning up merged branches...."
 git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -D
 
 # Clean branches that are gone 
+echo "cleaning up gone branches...."
 git branch -vv | grep ': gone]'|  grep -v "\*" | awk '{ print $1; }' | xargs git branch -D
