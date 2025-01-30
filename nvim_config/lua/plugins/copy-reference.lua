83bd08feb3e317dbfa3253ee06b1718be73d024f
Jed Palmater <TheJedinator@users.noreply.github.com>
2025-01-30 17:15:33 -0400

    add neovim config

---
+++ nvim_config/lua/plugins/copy-reference.lua
@@ -0,0 +1,12 @@
+return {
+  "ranelpadon/python-copy-reference.vim",
+  keys = {
+    {
+      "<leader>cp",
+      "<cmd>PythonCopyReferenceDotted<cr>",
+      desc = "Copy Python Reference",
+    },
+  },
+
+  filetypes = { "python" },
+}
---
+++ nvim_config/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      frecency = true,
+      formatters = {
+        file = {
+          filename_first = true,
+        },
+      },
+    },
+  },
+}
